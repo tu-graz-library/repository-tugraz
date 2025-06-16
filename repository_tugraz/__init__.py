7ed433a3ed58569990a7bb903eca4065b0663c63
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-06-16 14:51:39 +0200

    init: commit

---
+++ repository_tugraz/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# repository-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+"""Repository TUGraz."""
+
+__version__ = "0.0.1"
+
+__all__ = ("__version__",)
