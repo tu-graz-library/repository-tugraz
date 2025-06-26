444a504d8d809212b7529ab59db2dade57cb23f6
Laura <lthaci@tugraz.at>
2025-06-26 11:25:00 +0200

    feat: add ui helper class for common page imp

---
+++ data/urls.ts
@@ -0,0 +1,19 @@
+// -*- coding: utf-8 -*-
+//
+// Copyright (C) 2025 Graz University of Technology.
+//
+// repository-tugraz is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+
+// Direct URLS for a quick navigation
+const baseURL = "https://127.0.0.1/";
+
+export const urls = {
+  baseURL,
+  profileURL: `${baseURL}account/settings/profile`,
+  newUploadURL: `${baseURL}uploads/new`,
+  myDashboardURL: `${baseURL}me/uploads`,
+  communitiesURL: `${baseURL}communities`,
+  newCommunityURL: `${baseURL}communities/new`
+};
